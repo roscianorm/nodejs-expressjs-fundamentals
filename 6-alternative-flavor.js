6fc7bfaff37feb26f699241256763f4e2da53d27
Miguel Rosciano <roscianorm@gmail.com>
2021-07-11 15:21:57 -0600

    alternative flavor of export modules

--- 6-alternative-flavor.js
+++ 6-alternative-flavor.js
@@ -1,4 +1,8 @@
-const items = ['item1', 'item2']
+// const items = ['item1', 'item2']
+module.exports.items = ['item1', 'item2'] // Exportar directamente
 const person = {
   name: 'bob',
 }
+
+// Como el module es un objeto podemos asignarle una propiedad singlePerson con valor de person a la propiedad exports del module y asi exportarla
+module.exports.singlePerson = person
