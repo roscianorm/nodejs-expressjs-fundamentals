76d8efabad8bef67b686505479af15cd041d3059
Miguel Rosciano <roscianorm@gmail.com>
2021-07-11 14:38:14 -0600

    Globals done

---
+++ 2-globals.js
@@ -0,0 +1,22 @@
+// GLOBAL VARIABLES - NO WINDOW OBJECT !!!!
+
+// __dirname  - path to current directory
+// __filename - file name
+// require    - function to use modules (CommonJS)
+// modules    - info about current module (file)
+// process    - info about env where the program is being executed
+
+// MORE https://nodejs.org/api/globals.html
+
+console.log(__dirname)
+console.log(__filename)
+
+// Solo imprime una vez
+setTimeout(() => {
+  console.log('Hello World!')
+}, 500)
+
+// Imprime cada segundo
+setInterval(() => {
+  console.log('Hello World!')
+}, 1000)
--- app.js
+++
@@ -1,7 +0,0 @@
-// GLOBALS - NO WINDOW !!!!
-
-// __dirname  - path to current directory
-// __filename - file name
-// require    - function to use modules (CommonJS)
-// modules    - info about current module (file)
-// process    - info about env where the program is being executed
