03843119373726755495e6dd4d2935006055b614
Miguel Rosciano <roscianorm@gmail.com>
2021-07-12 08:59:38 -0600

    starting event loop

---
+++ 1-event-loop-examples/3-setInterval.js
@@ -0,0 +1,10 @@
+setInterval(() => {
+  console.log('hello world')
+}, 2000)
+console.log(`I will run first`)
+console.log(`I will run first`)
+console.log(`I will run first`)
+console.log(`I will run first`)
+// process stays alive unless
+// Kill Process CONTROL + C
+// unexpected error
