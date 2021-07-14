9788db7ce59f56c95f8312005af4a87cf7b8309b
Miguel Rosciano <roscianorm@gmail.com>
2021-07-13 21:48:17 -0600

    Express baby

---
+++ 1-node-tutorial/18-streams-all.js
@@ -0,0 +1,31 @@
+const http = require('http')
+const { readFileSync, writeFileSync, createReadStream } = require('fs')
+
+// for (let i = 0; i < 100000; i++) {
+//   writeFileSync('./content/big.txt', `Hello World! ${i}\n`, { flag: 'a' })
+// }
+// console.log('End of the file')
+
+// const stream = createReadStream('./content/big.txt', {
+//   highWaterMark: 90000,
+//   encoding: 'utf8',
+// })
+
+// stream.on('data', (result) => {
+//   console.log(result)
+// })
+// stream.on('error', (err) => console.log(err))
+
+http
+  .createServer((req, res) => {
+    // const text = readFileSync('./content/big.txt', 'utf8')
+    // res.end(text)
+    const fileStream = createReadStream('./content/big.txt', 'utf8')
+    fileStream.on('open', () => {
+      fileStream.pipe(res)
+    })
+    fileStream.on('error', (err) => {
+      res.end(err)
+    })
+  })
+  .listen(5000)
---
+++ 2-express-basics/final/03-express-basics.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const app = express()
+
+// Home
+app.get('/', (req, res) => {
+  res.status(200).send('Welcome to the home page!')
+})
+// About
+app.get('/about', (req, res) => {
+  res.status(200).send('Welcome to the about page!')
+})
+// 404
+// Este va a ser como el default, todos los request que no esten configurados caeran aca
+app.all('*', (req, res) => {
+  res.status(404).send('<h1>Welcome to the 404 page!</h1>')
+})
+
+app.listen(5000, () => {
+  console.log(`Server is listening on port 5000...`)
+})
+
+// app.get
+// app.post
+// app.put
+// app.delete
+// app.all
+// app.use
+// app.listen
+
+// app.get - Recibe como parametro la ruta a la que va a tratar y la funcion callback de que le indica loq ue debe hacer
+
+// app.post
+
+// app.put
+
+// app.delete
+
+// app.all - Este va a ser como el default, todos los request que no esten configurados caeran aca
+
+// app.use
+
+// app.listen - Recibe como parametro el puerto por el que el servidor va a escuchar
