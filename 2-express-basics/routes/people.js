1aa92a10f924d7917ec001be79df678e903c1c93
Miguel Rosciano <roscianorm@gmail.com>
2021-07-15 17:25:43 -0600

    Listo express

---
+++ 2-express-basics/routes/people.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const {
+  getPeople,
+  createPerson,
+  createPersonPostman,
+  updatePerson,
+  removePerson,
+} = require('../controllers/people')
+
+const router = express.Router()
+
+// router.get('/', getPeople)
+// router.post('/', createPerson)
+// router.post('/postman', createPersonPostman)
+// router.put('/:id', updatePerson)
+// router.delete('/:id', removePerson)
+
+// ----------------------------------------------------------------
+
+router.route('/').get(getPeople).post(createPerson)
+router.route('/postman').post(createPersonPostman)
+router.route('/:id').put(updatePerson).delete(removePerson)
+
+module.exports = router
